31180b857d5ea03cc72f37cd30beb1e319702b3f
William Kopaczewski <w_kopa@yahoo.com>
2025-06-30 21:23:29 -0400

    added player sprite and rotation

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
 
 from constants import *
+from sprites.player import player
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player_1 = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player_1.update(dt)
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
